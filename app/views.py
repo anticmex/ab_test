5178024c34e63915af2834c41846b2d53a944256
Yury Dmitriev <anticmex2@gmail.com>
2020-10-15 22:46:52 +0300

    modifed1

--- app/views.py
+++ app/views.py
@@ -15,15 +15,6 @@ counter_click = Counter()
 
 def index(request):
     # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
-
-
-    from_landing = request.GET.get('from-landing', '')
-    if from_landing == 'test':
-        counter_show.update({'test_from_landing': 1})
-        counter_click.update({'click': 1})
-    elif from_landing == 'original':
-        counter_show.update({'original_from_landing': 1})
-        counter_click.update({'click': 1})
     return render(request, 'index.html')
 
 
@@ -32,6 +23,8 @@ def landing(request):
     # в зависимости от GET параметра ab-test-arg
     # который может принимать значения original и test
     # Так же реализуйте логику подсчета количества показов
+    counter_show.update({'original_from_landing': 1})
+    counter_click.update({'click': 1})
     return render(request, 'landing.html')
 
 
@@ -40,13 +33,22 @@ def alternate(request):
     # в зависимости от GET параметра ab-test-arg
     # который может принимать значения original и test
     # Так же реализуйте логику подсчета количества показов
+    counter_show.update({'test_from_landing': 1})
+    counter_click.update({'click': 1})
     return render(request, 'landing_alternate.html')
 
 
 def stats(request):
     # Реализуйте логику подсчета отношения количества переходов к количеству показов страницы
     # Для вывода результат передайте в следующем формате:
+    
+    if counter_click['click'] > 0:
+        stat_origin = counter_show['original_from_landing']/counter_click['click']
+        stat_test = counter_show['test_from_landing']/counter_click['click']
+    else:
+        return HttpResponse('Страницы АБ-тестирования не посещались!')
+
     return render(request, 'stats.html', context={
-        'test_conversion': counter_show['test_from_landing']/counter_click['click'],
-        'original_conversion': counter_show['original_from_landing']/counter_click['click'],
+        'test_conversion': stat_test,
+        'original_conversion': stat_origin,
     })
